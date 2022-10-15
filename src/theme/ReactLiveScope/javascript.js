ddc889b61c1337bae23f785106c8fd84f557b38f
Gaël Blanchemain <anegg0@users.noreply.github.com>
2022-10-15 09:55:38 -0700

    Added JS Snippets to live code block

---
+++ src/theme/ReactLiveScope/javascript.js
@@ -0,0 +1,16 @@
+import CodeBlock from '@theme/CodeBlock';
+
+export default function MyReactPage() {
+  return (
+    <div>
+      <CodeBlock
+        language="jsx"
+        title="/src/components/HelloCodeTitle.js"
+        showLineNumbers>
+        {`function HelloCodeTitle(props) {
+  return <h1>Hello, {props.name}</h1>;
+}`}
+      </CodeBlock>
+    </div>
+  );
+}
